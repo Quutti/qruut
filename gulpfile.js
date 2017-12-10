70140f47df612cfc64b1c89f867ef2f57aeb7a19
Jokke Liikkanen <quutticode@gmail.com>
2017-12-10 22:25:08 +0200

    Add dependencies, gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require("gulp");
+const typescript = require("gulp-typescript");
+const clean = require("gulp-clean");
+const merge = require("merge2");
+
+const src = "./src";
+const dest = "./dist";
+
+function getSrc(ext) {
+    ext = Array.isArray(ext) ? ext : [ext];
+    return ext.map(e => `${src}/**/*.${e}`);
+}
+
+function compileTypescript() {
+    const tsProject = typescript.createProject("./tsconfig.json");
+    return gulp.src(getSrc(["ts", "tsx"]))
+        .pipe(tsProject())
+        .pipe(gulp.dest(dest, { base: "." }))
+};
+
+function copyCss() {
+    return gulp.src(getSrc("css"))
+        .pipe(gulp.dest(dest, { base: "." }));
+}
+
+gulp.task("build", ["clean"], () => {
+    return merge([
+        compileTypescript(),
+        copyCss()
+    ]);
+});
+
+gulp.task("clean", () => {
+    return gulp.src(dest, { read: false })
+        .pipe(clean({ force: true }));
+});
