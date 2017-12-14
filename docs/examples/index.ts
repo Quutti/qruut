706d80bbc0bbc37231ac2d5826d352a3e3c3db74
Jokke Liikkanen <quutticode@gmail.com>
2017-12-14 22:11:43 +0200

    Add example for List component

--- docs/examples/index.ts
+++ docs/examples/index.ts
@@ -2,3 +2,4 @@ export * from "./button/button.example";
 export * from "./card/card.example";
 export * from "./grid/grid.example";
 export * from "./table/table.example";
+export * from "./list/list.example";
