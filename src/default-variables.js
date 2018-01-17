aa5e15d18285a56eab7412653cf343bc80c78306
Jokke Liikkanen <quutticode@gmail.com>
2018-01-17 22:27:44 +0200

    Change default shadow style

--- src/default-variables.js
+++ src/default-variables.js
@@ -23,7 +23,7 @@ function getVariables() {
         "font-family": "Roboto",
         "font-size": "16px",
         "color": "#333",
-        "shadow": "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)"
+        "shadow": "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)"
     });
 
     // Button
